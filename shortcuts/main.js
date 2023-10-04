a15d4fef7f8895eba26479e71e2081fa7e556473
Tushar Chandra <me@tusharc.dev>
2023-10-03 20:26:25 -0500

    refactor: rewrite with what I learned from the birthday event

--- shortcuts/main.js
+++ shortcuts/main.js
@@ -1,56 +1,60 @@
-// Modify as desired; another combination is
-// { back: 38, left: 37, right: 39 } for up/left/right arrows
+// @ts-check
+window.addEventListener('DOMContentLoaded', main);
+
+/** Type definitions for help when in VSCode
+ * @typedef {{back: number, left: number, right: number}} ActionsMap
+ * @typedef {Record<string, number>} Keycodes
+ */
+
+/** @type {Keycodes} */
 const KEYCODES = {
-  back: 73,  // i
-  left: 74,  // j
-  right: 75,  // k
+  f: 70,
+  i: 73,
+  j: 74,
+  k: 75,
+  l: 76,
+  left: 37,
+  up: 38,
+  right: 39,
+  down: 40,
 };
-const ALTKEYCODES = {
-  back: 38,  // up arrow
-  left: 37,  // left arrow
-  right: 39,  // right arrow
+
+/** @type {ActionsMap} */
+const arrowActions = {
+  back: KEYCODES.up,
+  left: KEYCODES.left,
+  right: KEYCODES.right,
 };
-const PAGES = {
-  CE_MAIN: "https://www.clickcritters.com/clickexchange.php",
-  FEED: "https://www.clickcritters.com/feed/"
-}
 
-let leftAnswer, rightAnswer;
-let feedLink;
-window.onload = () => {
-  const width = document.body.clientWidth;
-  const xLeft = (width / 2) - 50;
-  const xRight = (width / 2) + 50;
+/** @type {ActionsMap} */
+const ijkActions = {
+  back: KEYCODES.i,
+  left: KEYCODES.j,
+  right: KEYCODES.l,
+};
 
-  // Check if we're on the main CE page; if so, there's a lot of extra text at the top
-  // and we have to account for this when setting the y-coordinate.
-  const y = (document.URL === PAGES.CE_MAIN) ? 350 : 150;
+const URLS = {
+  CE_MAIN: 'https://www.clickcritters.com/clickexchange.php',
+  FEED: 'https://www.clickcritters.com/feed/',
+};
 
-  leftAnswer = document.elementFromPoint(xLeft, y);
-  rightAnswer = document.elementFromPoint(xRight, y);
-  feedLink = document.querySelector("a[href*='?act=doRareCandy']");
-}
+function clickLink(e) {
+  // Find the offset of the first link on the page
+  // This is 0 on the 'correct answer' page and 1 on the main CE page, which we
+  // operationalize by looking at classes (on the main page, the first link is a plain <a>)
+  const offset = document.links[0].classList.length ? 0 : 1;
 
-const clickLink = (e) => {
-  if (e.keyCode === KEYCODES.left) {
-    leftAnswer.click();
+  // This supports both arrows and ijk keys because I use them interchangeably
+  ['back', 'left', 'right'].forEach((action, index) => {
+    if (e.keyCode === arrowActions[action]) {
+      document.links[index + offset].click();
+    }
+  });
+
+  // Oh, and add the feed page; f clicks the link
+  if (e.keyCode === KEYCODES.f) {
+    const feedLink = document.querySelector("a[href*='?act=doRareCandy']");
+    if (feedLink) feedLink.click();
     return;
   }
-  if (e.keyCode === KEYCODES.right) {
-    rightAnswer.click();
-    return;
-  }
-  if (e.keyCode === KEYCODES.back) {
-    document.links[0].click();
-    return;
-  }
-
-  if (e.keyCode === 70 && document.URL.startsWith(PAGES.FEED)) {
-    console.log("beep");
-    feedLink.click();
-  }
-}
-
-if (window === top) {
-  window.addEventListener('keyup', clickLink, false);
 }
